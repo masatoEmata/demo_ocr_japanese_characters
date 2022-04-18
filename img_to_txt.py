40b5f2a2795ad5a472af1ff7014be64b118db96f
masatoEmata <lh57.nm@gmail.com>
2022-04-18 16:05:08 +0900

    refactorImageToTxt

--- img_to_txt.py
+++ img_to_txt.py
@@ -1,34 +1,49 @@
 from PIL import Image
 from pathlib import Path
 import glob
-
 import pyocr
 import cv2
 
-tools = pyocr.get_available_tools()
-tesseract = tools[0]
-# print(tools)
 
+class ImageToTxt:
+    PATH_PATTERN = './input/img/*.jpg'
+    LAYOUT_RANGE = 13
+    LANG = 'jpn'
 
-# PDFファイルのパスを取得し順番に捌いていく
-for x in glob.glob("./input/img/*sample_2.jpg"):
-    path = Path(x)
-    # img = Image.open(path)
-    im_gray = cv2.imread(str(path), 0)
-    path_gray = f'./input/img/img_gray/{path.name}'
-    cv2.imwrite(path_gray, im_gray)
-    img = Image.open(path_gray)
+    def __init__(self) -> None:
+        tools = pyocr.get_available_tools()
+        self.tesseract = tools[0]
 
-    # img.show()
-    for i in range(13):
-        print('######################')
-        print(f'METHOD NUMBER: {i}')
+    def open_img(self, path: Path):
+        # img = Image.open(path)
+        im_gray = cv2.imread(str(path), 0)
+        path_gray = f'./input/img/img_gray/{path.name}'
+        cv2.imwrite(path_gray, im_gray)
+        img = Image.open(path_gray)
+        return img
+
+    def img_to_str(self, img: Image, tesseract_layout: int):
+        print(f'METHOD NUMBER: {tesseract_layout}')
         try:
-            txt = tesseract.image_to_string(
-                img, lang='jpn', builder=pyocr.builders.TextBuilder(tesseract_layout=i)
+            txt = self.tesseract.image_to_string(
+                img, lang=self.LANG, builder=pyocr.builders.TextBuilder(tesseract_layout=tesseract_layout)
             )
-            print(f'FILE NAME: {path.name}')
             print(f'TXT: {txt}')
         except Exception as e:
             print('NOT FOUND')
-        print('######################')
+
+    def all_layout_img_to_str(self, img):
+        for i in range(self.LAYOUT_RANGE):
+            self.img_to_str(img, i)
+
+    def all_img_to_str(self):
+        for path_str in glob.glob(self.PATH_PATTERN):
+            path = Path(path_str)
+            print(f'FILE NAME: {path.name}')
+            img = self.open_img(path)
+            self.all_layout_img_to_str(img)
+
+
+if __name__ == '__main__':
+    i2t = ImageToTxt()
+    i2t.all_img_to_str()
--- img_to_txt_b.py
+++ img_to_txt_b.py
@@ -1,27 +1,37 @@
 import sys
 import pyocr
-
 from PIL import Image
 import pyocr
 import pyocr.builders
 
-tools = pyocr.get_available_tools()
 
-#OCRが使えるかチェック
-if len(tools) == 0:
-    print('OCRツールが使えません')
-    sys.exit(1)
+class ImgToTxtB:
+    PATH_STR = rf'./input/img/message_sample.png'
+    LAYOUT = 11
+    LANG = 'script/Japanese'
 
-tool = tools[0]
-print("インストールされているOCRツールは', %s" % (tool.get_name()) ,'です。\n Tesseract（テッセラクト）は光学文字認識のエンジンです。\n\n') 
+    def __init__(self) -> None:
+        tools = pyocr.get_available_tools()
+        self.is_ocr_available(tools)
+        self.tool = tools[0]
 
-langs = tool.get_available_languages()
-print(langs,'などの言語を指定できます。')
+    def is_ocr_available(self, tools):
+        if len(tools) == 0:
+            print('Cannot use ocr tools.')
+            sys.exit(1)
 
-# OCRを実行する画像イメージや言語指定、オプション指定
-txt = tool.image_to_string( 
-    Image.open(rf"./input/img/message_sample.png"),  # ここを変更
-    lang='script/Japanese',
-    builder=pyocr.builders.TextBuilder(tesseract_layout=11) #オプション番号は必要に応じて変更してください。デフォルトは「3」
-)
-print('\n\nOCR（光学文字認識）の実行結果\n\n\n__________________\n\n',txt, '\n\n__________________\n\n')
+    def show_available_ocr_tools(self):
+        names = self.tool.get_name()
+        print(names)
+        return names
+
+    def show_available_languages(self):
+        langs = self.tool.get_available_languages()
+        print(langs)
+        return langs
+    
+    def main(self):
+        txt = self.tool.image_to_string(
+            Image.open(self.PATH_STR), lang=self.LANG, builder=pyocr.builders.TextBuilder(tesseract_layout=self.LAYOUT)
+        )
+        print('\n\nOCR RESULT\n\n\n__________________\n\n',txt, '\n\n__________________\n\n')
